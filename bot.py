564327cc27b12596289c05a67ad73249cd94b58d
Daresian <137519934+Daresian@users.noreply.github.com>
2026-04-06 16:31:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import (
 
 TOKEN = "8308224905:AAFp1h0eZgqfnAxtv_u94VkXh2bVxwRRJOU"
 ADMIN_ID = 123456789
-PAYPAL_LINK = "https://paypal.me/bucefalo74”
+PAYPAL_LINK = "https://paypal.me/bucefalo74"
 
 logging.basicConfig(level=logging.INFO)
 
