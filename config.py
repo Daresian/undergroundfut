ed214d53ee979c22e85f88b18c7d11f2e7ab3a86
Daresian <137519934+Daresian@users.noreply.github.com>
2026-04-10 13:54:01 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,9 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")  # Configúralo en Railway
+ADMINS = [123456789]  # Sustituye por tu ID de Telegram
+GROUP_ID = -1001234567890  # ID del grupo
+PAYPAL_LINK = "https://paypal.me/bucefalo74"
